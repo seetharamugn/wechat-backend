9a6c3928268f51e64624244435049992b04a5e57
seetharamugn1 <seetharamu@pepul.com>
2023-08-23 21:39:05 +0530

    added the id to the struct

--- models/Chat.go
+++ models/Chat.go
@@ -1,12 +1,16 @@
 package models
 
-import "time"
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
 
 type Chat struct {
-	UserNumber  []interface{} `bson:"userNumber" json:"userNumber"`
-	CreatedBy   string        `bson:"createdBy" json:"createdBy"`
-	Status      string        `bson:"status" json:"status"`
-	CreatedAt   time.Time     `bson:"createdAt" json:"createdAt"`
-	UpdatedAt   time.Time     `bson:"updatedAt" json:"updatedAt"`
-	LastMessage string        `bson:"lastMessage" json:"lastMessage"`
+	ID          primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
+	UserNumber  []interface{}      `bson:"userNumber" json:"userNumber"`
+	CreatedBy   string             `bson:"createdBy" json:"createdBy"`
+	Status      string             `bson:"status" json:"status"`
+	CreatedAt   time.Time          `bson:"createdAt" json:"createdAt"`
+	UpdatedAt   time.Time          `bson:"updatedAt" json:"updatedAt"`
+	LastMessage string             `bson:"lastMessage" json:"lastMessage"`
 }
