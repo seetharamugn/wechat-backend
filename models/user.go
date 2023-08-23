115c400c66e091e948b2470d31fa683d3c10d4c4
seetharamugn1 <seetharamu@pepul.com>
2023-08-23 21:51:16 +0530

    added the ref and the chatid is created

--- models/user.go
+++ models/user.go
@@ -1,18 +1,21 @@
 package models
 
-import "time"
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
 
 type User struct {
-	ID         string    `bson:"_id ,omitempty" json:"id"`
-	UserId     string    `bson:"userId" json:"userId"`
-	FirstName  string    `bson:"firstName" json:"firstName"`
-	LastName   string    `bson:"lastName" json:"lastName"`
-	Username   string    `bson:"userName" json:"userName"`
-	Password   string    `bson:"password" json:"password"`
-	Email      string    `bson:"email" json:"email"`
-	PhoneNo    string    `bson:"phoneNo" json:"phoneNo"`
-	UserActive bool      `bson:"userActive" json:"userActive"`
-	CreatedAt  time.Time `bson:"createdAt" json:"createdAt"`
-	UpdatedAt  time.Time `bson:"updatedAt" json:"updatedAt"`
-	DeletedAt  time.Time `bson:"deletedAt" json:"deletedAt"`
+	ID         primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
+	UserId     string             `bson:"userId" json:"userId"`
+	FirstName  string             `bson:"firstName" json:"firstName"`
+	LastName   string             `bson:"lastName" json:"lastName"`
+	Username   string             `bson:"userName" json:"userName"`
+	Password   string             `bson:"password" json:"password"`
+	Email      string             `bson:"email" json:"email"`
+	PhoneNo    string             `bson:"phoneNo" json:"phoneNo"`
+	UserActive bool               `bson:"userActive" json:"userActive"`
+	CreatedAt  time.Time          `bson:"createdAt" json:"createdAt"`
+	UpdatedAt  time.Time          `bson:"updatedAt" json:"updatedAt"`
+	DeletedAt  time.Time          `bson:"deletedAt" json:"deletedAt"`
 }
