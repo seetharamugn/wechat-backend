dbf6e1a1eca59d5e8b4b3550b7cf486ab2f0e314
seetharamugn1 <seetharamu@pepul.com>
2023-09-01 16:51:47 +0530

    added the media id and other detials proeprly to verify the incomming message

--- Dao/Response.go
+++ Dao/Response.go
@@ -30,20 +30,11 @@ type ResponseMessage struct {
 	}
 }
 
-//{
-//  "messaging_product": "whatsapp",
-//  "url": "<URL>",
-//  "mime_type": "<MIME_TYPE>",
-//  "sha256": "<HASH>",
-//  "file_size": "<FILE_SIZE>",
-//  "id": "<MEDIA_ID>"
-//}
-
 type ResponseMedia struct {
-	MessagingProduct string `json:"messaging_product"`
 	Url              string `json:"url"`
 	MimeType         string `json:"mime_type"`
 	Sha256           string `json:"sha256"`
-	FileSize         string `json:"file_size"`
+	FileSize         int    `json:"file_size"`
 	Id               string `json:"id"`
+	MessagingProduct string `json:"messaging_product"`
 }
