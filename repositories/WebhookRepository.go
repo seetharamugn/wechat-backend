1e437f8020923ce7d9dfc42840887683c52e66b3
seetharamugn1 <seetharamu@pepul.com>
2023-09-01 22:46:55 +0530

    incomming video added

--- repositories/WebhookRepository.go
+++ repositories/WebhookRepository.go
@@ -24,6 +24,7 @@ import (
 var ReplyUserCollection *mongo.Collection = initializers.OpenCollection(initializers.Client, "replyUser")
 
 func IncomingMessage(ctx *gin.Context, messageBody Dao.WebhookMessage) {
+	fmt.Println(messageBody)
 	if messageBody.Entry[0].Changes[0].Value.Messages[0].Type == "text" {
 		TextMessage(ctx, messageBody.Entry[0].Changes[0].Value.Messages[0].From,
 			messageBody.Entry[0].Changes[0].Value.Metadata.DisplayPhoneNumber,
