88bbf893dd5e24e42964478458e142d0e9c8d834
Seetharamu G N -X (segn - WIPRO LIMITED at Cisco) <segn@cisco.com>
2023-10-26 23:01:38 +0530

    removed the authentication

--- routers/UserRoutes.go
+++ routers/UserRoutes.go
@@ -3,14 +3,13 @@ package routers
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/seetharamugn/wachat/controllers"
-	"github.com/seetharamugn/wachat/middleware"
 )
 
 func UserRouter(routes *gin.RouterGroup) {
 	routes.POST("/signup", controllers.CreateUser)
-	routes.PUT("/update", middleware.ValidateAccessToken, controllers.Update)
-	routes.DELETE("/delete", middleware.ValidateAccessToken, controllers.Delete)
+	routes.PUT("/update", controllers.Update)
+	routes.DELETE("/delete", controllers.Delete)
 	routes.POST("/verifyEmail", controllers.VerifyEmail)
 	routes.POST("/resetPassword", controllers.ResetPassword)
-	routes.GET("/getUserDetails", middleware.ValidateAccessToken, controllers.GetUserDetails)
+	routes.GET("/getUserDetails", controllers.GetUserDetails)
 }
--- routers/WhatsappRoutes.go
+++ routers/WhatsappRoutes.go
@@ -3,9 +3,8 @@ package routers
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/seetharamugn/wachat/controllers"
-	"github.com/seetharamugn/wachat/middleware"
 )
 
 func WhatsappRouter(routes *gin.RouterGroup) {
-	routes.POST("/createAccount", middleware.ValidateAccessToken, controllers.CreateAccount)
+	routes.POST("/createAccount", controllers.CreateAccount)
 }
